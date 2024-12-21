cd83e17ca7a119b15a837fbf8cf3f3d251837177
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-21 14:58:19 +0900

    ci: test --replace

--- example/foo/aws_s3_bucket.tf
+++ example/foo/aws_s3_bucket.tf
@@ -1,7 +1,7 @@
-resource "aws_s3_bucket" "example-1" {
+resource "aws_s3_bucket" "example_1" {
   bucket = "test-1"
 }
 
-resource "aws_s3_bucket" "example-2" {
+resource "aws_s3_bucket" "example_2" {
   bucket = "test-2"
 }
