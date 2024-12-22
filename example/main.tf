e6f450401c14556b397341daf262b40710c7520c
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-22 09:34:32 +0900

    chore: fix example

--- example/main.tf
+++ example/main.tf
@@ -1,1 +1,13 @@
 resource "null_resource" "foo_prod" {}
+
+resource "github_repository" "example-1" {
+  name = "example-1"
+}
+
+resource "github_branch" "example" {
+  repository = github_repository.example-1.name
+  branch     = "example"
+  depends_on = [
+    github_repository.example-1
+  ]
+}
