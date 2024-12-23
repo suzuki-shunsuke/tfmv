ab48c5de11d502a15b0b533cb0b2c3c6b7105b7d
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-23 12:56:55 +0900

    chore: update example

--- example/main.tf
+++ example/main.tf
@@ -2,10 +2,19 @@ resource "github_repository" "example-1" {
   name = "example-1"
 }
 
-resource "github_branch" "example" {
+data "github_branch" "example-2" {
   repository = github_repository.example-1.name
   branch     = "example"
   depends_on = [
-    github_repository.example-1
+    github_repository.example-1,
+    module.example-3
   ]
+}
+
+module "example-3" {
+  source = "./foo/module"
+}
+
+output "branch_sha" {
+  value = data.github_branch.example-2.sha
 }
