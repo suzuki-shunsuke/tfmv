04c887e7ef7101239f566d26d0648ee979b821f8
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-23 09:01:25 +0900

    fix: fix dry run of hcledit

--- pkg/controller/hcledit.go
+++ pkg/controller/hcledit.go
@@ -27,18 +27,25 @@ type MoveBlockOpt struct {
 
 func (e *Editor) Move(logE *logrus.Entry, opt *MoveBlockOpt) error {
 	filter := editor.NewBlockRenameFilter(opt.From, opt.To)
+
+	if e.dryRun {
+		cl := editor.NewClient(&editor.Option{
+			InStream:  opt.Stdin,
+			OutStream: io.Discard,
+			ErrStream: e.stderr,
+		})
+		logE.Info("[DRY RUN] moving a block")
+		if err := cl.Edit(opt.FilePath, false, filter); err != nil {
+			return fmt.Errorf("move a block in %s from %s to %s: %w", opt.FilePath, opt.From, opt.To, err)
+		}
+		return nil
+	}
 	cl := editor.NewClient(&editor.Option{
 		InStream:  opt.Stdin,
 		OutStream: opt.Stdout,
 		ErrStream: e.stderr,
 	})
-
-	if e.dryRun {
-		logE.Info("[DRY RUN] move a block")
-		return nil
-	}
 	logE.Info("moving a block")
-
 	if err := cl.Edit(opt.FilePath, opt.Update, filter); err != nil {
 		return fmt.Errorf("move a block in %s from %s to %s: %w", opt.FilePath, opt.From, opt.To, err)
 	}
