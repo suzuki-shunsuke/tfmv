63f77a834eacb2b33444282b2e1e2245a5c723e6
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-12-23 20:20:58 +0900

    ci add test

---
+++ test/include-1/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+
+f=$(mktemp)
+cp main.tf "$f"
+tfmv -r '-/_' --include 'aws_'
+
+if ! diff main.tf "$f"; then
+  rm "$f"
+  echo "[ERROR] main.tf is changed" >&2
+  return 1
+fi
+if test -f moved.tf; then
+  rm "$f"
+  echo "[ERROR] moved.tf is created" >&2
+  return 1
+fi
+
+rm "$f"
+echo "[INFO] passed test" >&2
+exit 0
