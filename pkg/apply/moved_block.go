3f9ac7b08823c3ae6c1df38d0eafaba5c0ea22ee
Shunsuke Suzuki <suzuki-shunsuke@users.noreply.github.com>
2025-01-31 16:30:20 +0900

    ci: use autofix.ci (#96)
    * ci: use autofix.ci
    
    * test
    
    * [autofix.ci] apply automated fixes
    
    * fix: remove a leading newline
    
    * refactor: fix a lint error
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- pkg/apply/moved_block.go
+++ pkg/apply/moved_block.go
@@ -8,23 +8,36 @@ import (
 	"github.com/suzuki-shunsuke/tfmv/pkg/types"
 )
 
+var filePermission os.FileMode = 0o644 //nolint:gochecknoglobals
+
 func (a *Applier) writeMovedBlock(block *types.Block, movedFile string) error {
 	if block.IsData() {
 		return nil
 	}
-	file, err := a.fs.OpenFile(movedFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644) //nolint:mnd
-	if err != nil {
-		return fmt.Errorf("open a file: %w", err)
-	}
-	defer file.Close()
+
 	content := fmt.Sprintf(`moved {
   from = %s
   to   = %s
 }
 `, block.TFAddress, block.NewTFAddress)
-	if f, err := afero.Exists(a.fs, movedFile); err == nil && f {
-		content = "\n" + content
+
+	f, err := a.fs.Stat(movedFile)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return fmt.Errorf("check a file exists: %w", err)
+		}
+		// create a file
+		if err := afero.WriteFile(a.fs, movedFile, []byte(content), filePermission); err != nil {
+			return fmt.Errorf("create a moved block file: %w", err)
+		}
+		return nil
 	}
-	fmt.Fprint(file, content)
+	// update a file
+	file, err := a.fs.OpenFile(movedFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, f.Mode())
+	if err != nil {
+		return fmt.Errorf("open a file: %w", err)
+	}
+	defer file.Close()
+	fmt.Fprint(file, "\n"+content)
 	return nil
 }
